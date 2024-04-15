723027835485eb7e58411a1383068d1990d7ebb6
parsapanahpoor <parsapanahpoor77@gmail.com>
2024-04-15 19:29:10 +0330

    admin controller

---
+++ TokenBased-Authentication.Presentation/Areas/Admin/Controllers/v1/AdminBaseController.cs
@@ -0,0 +1,14 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using TokenBased_Authentication.Presentation.Filter;
+
+namespace TokenBased_Authentication.Presentation.Areas.Admin.Controllers.v1;
+
+[ApiController]
+[CatchExceptionFilter]
+
+public class AdminBaseController : ControllerBase
+{
+    private ISender? _mediator;
+    protected ISender Mediator => _mediator ??= HttpContext.RequestServices.GetRequiredService<ISender>();
+}
---
+++ TokenBased-Authentication.Presentation/Areas/Admin/Controllers/v1/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using EquipmentManagement.Presentation.HttpManager;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+namespace TokenBased_Authentication.Presentation.Areas.Admin.Controllers.v1;
+
+[ApiVersion("1")]
+[Route("api/v{version:apiVersion}/Admin/[controller]")]
+[Authorize]
+
+public class HomeController : AdminBaseController
+{
+	#region Admin Dashboard
+
+	[HttpGet("AdminDashboard")]
+	public async Task<IActionResult> AdminDashboard(CancellationToken cancellationToken)
+	{
+		return Ok(JsonResponseStatus.Success(null , "Wellcome to admin dashboard"));
+	}
+
+	#endregion
+}
